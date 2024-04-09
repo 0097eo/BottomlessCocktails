95d726d9e332f852f4b6335793abb27f144828d6
Emmanuel <emmanuelokello294@gmail.com>
2024-04-09 19:28:26 +0300

    adds funtion to fetch cocktails by name

--- src/script.js
+++ src/script.js
@@ -5,4 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(res => res.json())
         .then(data => data.drinks)
     }
+    //fetching a cocktail by name
+    function getDrinkByName(name){
+        return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+        .then(res => res.json())
+        .then(data => data.drinks)
+
+    }
 })
