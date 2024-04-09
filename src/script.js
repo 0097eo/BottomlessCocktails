d646b111982d9cf0bed9d6bbe685e64641945274
Emmanuel <emmanuelokello294@gmail.com>
2024-04-09 22:04:50 +0300

    uses try catch to handle errors

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     addCocktailImageStyles();
 
     // Function to fetch cocktails
-    const fetchCocktails = async () => {
+     const fetchCocktails = async () => {
         try {
             const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
             const data = await response.json();
@@ -44,31 +44,31 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    function displayCocktailDetails(cocktailId) {
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
-            .then(response => response.json())
-            .then(data => {
-                const cocktail = data.drinks[0];
-                document.getElementById("cocktails-div").innerHTML = '';
-                // Create and populate the detailed cocktail information
-                const cocktailDiv = document.createElement('div');
-                cocktailDiv.innerHTML = `
-                    <h2>${cocktail.strDrink}</h2>
-                    <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}" class="cocktail-image">
-                    <h2>Ingredients & Measurements</h2>
-                    ${cocktail.strMeasure1 ? `<p>${cocktail.strMeasure1} ${cocktail.strIngredient1}</p>` : ''}
-                    ${cocktail.strMeasure2 ? `<p>${cocktail.strMeasure2} ${cocktail.strIngredient2}</p>` : ''}
-                    ${cocktail.strMeasure3 ? `<p>${cocktail.strMeasure3} ${cocktail.strIngredient3}</p>` : ''}
-                    ${cocktail.strMeasure4 ? `<p>${cocktail.strMeasure4} ${cocktail.strIngredient4}</p>` : ''}
-                    <h2>Instructions</h2>
-                    <p>${cocktail.strInstructions}</p>
-                    <h2>Serve in A</h2>
-                    <p>${cocktail.strGlass}</p>
-                `;
-                document.getElementById("cocktails-div").appendChild(cocktailDiv);
-            });
-    }
-
+    const displayCocktailDetails = async (cocktailId) => {
+        try {
+            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`);
+            const data = await response.json();
+            const cocktail = data.drinks[0];
+            document.getElementById("cocktails-div").innerHTML = '';
+            const cocktailDiv = document.createElement('div');
+            cocktailDiv.innerHTML = `
+                <h2>${cocktail.strDrink}</h2>
+                <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}" class="cocktail-image">
+                <h2>Ingredients & Measurements</h2>
+                ${cocktail.strMeasure1 ? `<p>${cocktail.strMeasure1} ${cocktail.strIngredient1}</p>` : ''}
+                ${cocktail.strMeasure2 ? `<p>${cocktail.strMeasure2} ${cocktail.strIngredient2}</p>` : ''}
+                ${cocktail.strMeasure3 ? `<p>${cocktail.strMeasure3} ${cocktail.strIngredient3}</p>` : ''}
+                ${cocktail.strMeasure4 ? `<p>${cocktail.strMeasure4} ${cocktail.strIngredient4}</p>` : ''}
+                <h2>Instructions</h2>
+                <p>${cocktail.strInstructions}</p>
+                <h2>Serve in A</h2>
+                <p>${cocktail.strGlass}</p>
+            `;
+            document.getElementById("cocktails-div").appendChild(cocktailDiv);
+        } catch (error) {
+            console.error('Error fetching cocktail details:', error);
+        }
+    };
     // Function to search for a cocktail by name
     function getDrinkByName(name) {
         return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
